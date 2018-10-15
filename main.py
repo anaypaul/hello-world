7408c31f0b06acf208d0b6d15a72bffa24dad613
Anay Paul <anay.paul2@gmail.com>
2018-10-14 18:35:52 -0700

    main.py modified

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from sort import mergesort
 def sayHello():
 	print("Hello, World!")
 	print("file edit on web")
 
 def main():
 	sayHello()
+	arr = list(map(int, input().split()))
+	print(arr)
+	mergesort(arr,0,len(arr)-1)
+	print(arr)
+
 
 if __name__ == '__main__':
 	main()
